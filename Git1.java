0b09703546ec99bfa05dbbab916b68533e17b476
gireesh2017 <madakagireesh@gmail.com>
2017-09-14 16:54:09 +0530

    adding the Git1 file

---
+++ Git1.java
@@ -0,0 +1,9 @@
+class Git1
+{
+	public static void main (String args[])
+	{	
+		System.out.println("*********************");
+		System.out.println("Gireesh");
+		System.out.println("*********************");
+	}	
+}
