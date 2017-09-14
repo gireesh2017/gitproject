d55685413888444d88578577280871b052c1b6bf
gireesh2017 <madakagireesh@gmail.com>
2017-09-14 17:00:09 +0530

    adding modified Git1 file

--- Git1.java
+++ Git1.java
@@ -2,8 +2,8 @@ class Git1
 {
 	public static void main (String args[])
 	{	
-		System.out.println("*********************");
+		System.out.println("			");
 		System.out.println("Gireesh");
-		System.out.println("*********************");
+		System.out.println("			");
 	}	
 }
